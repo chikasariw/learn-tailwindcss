a6142cbcaa4c855cefa2687d57f4c2966f00bc26
chiisaww <chikasariw@gmail.com>
2024-01-29 20:03:02 +0700

    first commit

---
+++ belajar/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins'],
+      },
+    },
+  },
+  plugins: [],
+}
+
